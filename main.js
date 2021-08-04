9650348c97a124f5979709fb70a266d91040a1c6
Lior-Yosef <lioryosef06@gmail.com>
2021-08-05 01:47:40 +0300

    commit

---
+++ main.js
@@ -0,0 +1,209 @@
+
+// -------------------- mission one --------------------
+
+// var numOne =+prompt("type your first number");
+// var numTwo=+prompt("type your second number");
+
+// // console.log(`number one is ${numOne}`);
+// // console.log(`number two is ${numTwo}`);
+
+// var result = numOne+numTwo;
+// if (result>13){
+//     console.log("bar-mitzvah");
+// } else if (result==12){
+//     console.log("bat-mitzvah");
+// } else {
+//     console.log("this is a kid");
+// }
+
+// --------------------------------------------------
+
+
+
+
+// -------------------- mission two --------------------
+
+
+// var i= 0;
+// while ( i<=100){
+//     console.log("yes");
+//     i++
+// }
+
+// var numFromUser = Number (prompt ("select a number"));
+// var i =0;
+// while (i<numFromUser){
+//     console.log("no");
+//     i++;
+// }
+
+
+// --------------------------------------------------
+
+
+
+// -------------------- mission three --------------------
+
+// for (var i =0; i <1000 ; i++ ) {
+//     console.log(i,"javascript is not JAVA");
+// }
+
+// --------------------------------------------------
+
+
+
+
+// -------------------- mission four --------------------
+
+// var UserName = prompt ("what your name?");
+// var userNum = Number (prompt ("what your age?"));
+
+// for (var i=0; i<userNum; i++){
+//     console.log(UserName);
+// }
+
+// --------------------------------------------------
+
+
+// -------------------- mission five --------------------
+
+// var UserName = prompt ("what your name?");
+// var ageNum = Number (prompt ("what your age?"));
+// var email = prompt ("what your email?");
+
+// // console.log(`my first name is ${UserName}.\n my email is ${email}.\n my age is ${ageNum}.\n`);
+
+// var numTwo = Number (prompt ("type second number"));
+// if (ageNum>18){
+// for (var i=0 ; i<numTwo;i++){
+//     console.log(i, `my first name is ${UserName}.\n my email is ${email}.\n my age is ${ageNum}.\n`);
+// }}
+
+
+// // --------------------------------------------------
+
+
+// -------------------- mission six --------------------
+
+// var userName
+// myName= "lior"
+// num = Number (prompt( "select number "));
+
+// for (var i=0; i<num ; i++){
+//     userName=prompt("select name")
+//     if (userName==myName){
+//         console.log(`hi boss ${myName}`)
+//     }
+// }
+
+
+
+// // --------------------------------------------------
+
+
+
+
+// -------------------- mission seven --------------------
+
+
+// var numOne , numTwo
+
+// numOne = Number (prompt("Select number one"));
+// numTwo = Number (prompt("Select number two"));
+
+// for (var i=0 ; i<numOne; i++){
+// for (var p=0; p<numTwo; p++)
+// console.log (i*p);
+// }
+
+
+// // --------------------------------------------------
+
+
+
+
+// -------------------- mission eight --------------------
+
+// var numArray = [ 5,4,2,6,9] ;
+
+// for (var i=0; i<numArray.length ; i++) {
+//     console.log( numArray[i]);
+// }
+
+
+// ----------------------------------------------------------
+
+
+// -------------------- mission nine --------------------
+
+
+// var userName =[ "lior","avi","ori","or","jon"];
+// for ( var i=0; i<userName.length ; i++){
+//     console.log(userName [i]);
+// }
+
+
+// ----------------------------------------------------------
+
+
+
+// -------------------- mission tan --------------------
+
+// var userNum = [ 30,43,45,76,77,98,12,33];
+// for (var i=0; i<userNum.length ; i++){
+//     if(userNum[i]%2==0){
+//         console.log(userNum[i]);
+//     }
+// }
+
+// ----------------------------------------------------------
+
+
+
+
+// // -------------------- mission eleven --------------------
+
+// userName =[];
+
+// for(var i=0; i<5; i++){
+//     userName [i]=prompt  ( "give me name");{
+//         console.log(userName);
+//     }
+// }
+    
+// ----------------------------------------------------------
+
+
+
+// // -------------------- mission Twelve --------------------
+
+
+
+// userNum =[];
+
+//  for (var i=0; i<7; i++){
+//      userNum[i]=Number(prompt("give me seven numbers"));{
+//          if ( userNum[i]%2!=0 ){
+//              console.log(userNum[i]);
+//          }
+//      }
+//  }
+
+    
+// ----------------------------------------------------------
+
+
+
+
+
+
+
+
+// צרו מערך אקראי של 6 מספרים ומילים. צרו לולאה שעוברת על המערך ומחליפה את המילים במילה 
+
+// var numAndwords = ["yoni", 25, "oshri", 24,"lior" , 24];
+// for(var i=0; i<numAndwords.length;i++){
+//     if(numAndwords[i] *0 !=0){
+//         numAndwords[i]= "not a number"
+//     }
+// }
